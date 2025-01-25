de5ad633fc09e935640dca678d947584e6bf4c95
Rohit R <rohit02r@gmail.com>
2025-01-25 23:36:12 +0530

    fourth commit

--- scraper_project/settings.py
+++ scraper_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4sriJ402hg6fAyKbzLMVewwDXV0_5zULfi9DPJIcpW5SGimwYoYgwHNSltjTKYA5P_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','.vercel.app']
 
 
 
